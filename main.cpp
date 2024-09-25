bbc05ca97400058e92976e2a936f028277ece1e2
cobralicious <136372594+cobralicious@users.noreply.github.com>
2024-09-25 09:16:00 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,82 @@
+// #include <iostream>
+// using namespace std;
+//
+// void MultiplyNumbers()
+// {
+//     cout << "Input first num: ";
+//     int firstNumber = 0;
+//     cin >> firstNumber;
+//
+//     cout << "Input second num: " << endl;
+//     int secondNumber = 0;
+//     cin >> secondNumber;
+//
+//     int myltipliResult = firstNumber*secondNumber;
+//
+//     cout << firstNumber << " x " << secondNumber;
+//     cout << " = " << myltipliResult;
+// }
+//
+// int main()
+// {
+//     cout << "Программа для умножения двух чисел" << endl;
+//
+//     MultiplyNumbers();
+//
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+//
+// // Три глобальные целочисленные переменные
+// int firstNumЬer = 0;
+// int secondNumЬer = 0;
+// int multiplicationResult = 0;
+//
+// void MultiplyNumЬers()
+// {
+//     cout << "Введите первое число: ";
+//     cin >> firstNumЬer;
+//
+//     cout << "Введите второе число: ";
+//     cin >> secondNumЬer;
+//
+//     //Умножение двух чисел, сохранение результата в переменной
+//     multiplicationResult = firstNumЬer * secondNumЬer;
+//
+//     // Вывод результата
+//     cout << "Вывод из Mul tiplyNumЬers () : ";
+//     cout << firstNumЬer << " х " << secondNumЬer;
+//     cout << " = " << multiplicationResult << endl;
+// }
+//
+// int main ()
+// {
+//     cout << "Программа для умножения двух чисел"<< endl;
+//
+//     //Вызов функции, выполняющей всю работу
+//     MultiplyNumЬers ();
+//
+//     cout << "Вывод из main () : ";
+//
+//     // Теперь эта строка компилируется и работает!
+//     cout << firstNumЬer << " х " << secondNumЬer;
+//     cout << " = " << multiplicationResult << endl;
+//
+//     return 0;
+// }
+
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    const double pi = 3.1415926;
+    cout << "Value of pi is: " << pi << endl;
+
+    return 0;
+}
